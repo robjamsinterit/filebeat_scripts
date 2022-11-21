bbfeb70e465d0163e811e52cf2423b7435fb0d61
Robert Jamróz <robert.jamroz@sinterit.com>
2022-11-21 10:29:16 +0100

    filebeat sinterit config

--- filebeat.sh
+++ filebeat.sh
@@ -4,6 +4,10 @@ export GODEBUG='madvdontneed=1'
 export SERIAL_NUMBER="$(jq -r '.[] | select(.name=="/Hardware/Hardware/0/SerialNumber").value' /home/sinterit/global_parameters.json)"
 export PRINTER_NAME="$(jq -r '.[] | select(.name=="/Hardware/Hardware/0/PrinterName").value' /home/sinterit/global_parameters.json)"
 
+export CLUSTER_UUID="$(curl -k -s https://elastic:elastic@10.235.207.54:9200/ | jq -r .cluster_uuid)"
+export BEAT_USER="beats_system"
+export BEAT_PASS="beats_system"
+
 printenv
 
 /home/sinterit/bin/filebeat/filebeat --environment systemd \
